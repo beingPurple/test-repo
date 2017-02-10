1336dc49e8342a5ae38f2de72138bfcee1430d74
Joanna Wang <qiaohong_wang@bullis.org>
2017-02-10 16:52:46 -0500

    updated collecting system and shooter

--- ShooterAndCollecting.java
+++ ShooterAndCollecting.java
@@ -7,24 +7,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 /**
  * Created by Joanna and Gemma on 1/24/17.
  */
-
+//recent
 @TeleOp (name = "Green Shooter and Collector", group ="Green")
 
 public class ShooterAndCollecting extends OpMode {
-    DcMotor shoot1;
-    DcMotor shoot2;
+    DcMotor shooter;
+
     DcMotor collect;
 
-    private double speedcol = -0.8;
-    private double speedshoot = 1.0;
+    private double speedcol = -2.0;
+    private double speedshoot = 0.6;
     private double col = 0;
-    private double sh1 = 0;
-    private double sh2 = 0;
+    private double shooting = 0;
+
 
     public void init() {
 
-//        shoot1 = hardwareMap.dcMotor.get("sho");
-//        shoot2 = hardwareMap.dcMotor.get("shooter2");
+        shooter = hardwareMap.dcMotor.get("sho");
+
         collect = hardwareMap.dcMotor.get("co up");
         telemetry.addData("Get started!", 6);
     }
@@ -33,7 +33,8 @@ public class ShooterAndCollecting extends OpMode {
         telemetry.addData("Start the game", 4);
 
         //code for collecting (press button x):
-        if (gamepad1.x) {
+        //if(gamepad1.x){
+        if (gamepad2.x) {
             col = speedcol;
             telemetry.addData("collecting", col);
             collect.setPower(col);
@@ -44,18 +45,15 @@ public class ShooterAndCollecting extends OpMode {
 
 
         //code for shooters(press botton y)
-        if (gamepad1.y) {
-            sh1 = speedshoot;
-            sh2 = -speedshoot;
-            shoot1.setPower(sh1);
-            shoot2.setPower(sh2);
-            telemetry.addData("Shooter1", sh1);
-            telemetry.addData("Shooter2", sh2);
+        // if(gamepad1.y){
+        if (gamepad2.y) {
+            shooting = speedshoot;
+            telemetry.addData("Shooter", shooting);
+            shooter.setPower(shooting);
         } else {
-            sh1 = 0;
-            sh2 = 0;
-            shoot1.setPower(sh1);
-            shoot2.setPower(sh2);
+            shooting = 0;
+            shooter.setPower(shooting);
+
 
         }
     }
